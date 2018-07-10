bd2f80e3d877ff9372060100fd1ef43b2bf3070c
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:24:26 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,15 +2,21 @@
 # Code your methods below
 def starting_stance 
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
+end  
 def base_footwork 
   puts "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
+end
 def skip_step(starting_foot) 
   puts "Lower left foot to the ground,bounce left foot back up slightly, kicking it a few inches back,left, Right, Left and Left"
+end
 def bob_the_reins 
   puts "Holding your arms out in front of you, straight and at chest level, Cross your right wrist over your left and hold them together, Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
 def lasso 
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left, Lift right arm up and point right elbow diagonally to the right, Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
   def bust_a_move
     puts "Be cool, be snazzy!"
-  end
+end
+  
   starting_stance base_footwork skip_step(starting_foot) bob_the_reins lasso bust_a_move
