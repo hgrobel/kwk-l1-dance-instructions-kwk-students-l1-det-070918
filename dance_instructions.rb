8556fb9a2bc7b87444c47f6f3712ad4f367193ec
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:18:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,4 +5,4 @@ def dance_instructions
   base_footwork = "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
   skip_step(starting_foot) = "Lower left foot to the ground,bounce left foot back up slightly, kicking it a few inches back,left, Right, Left and Left"
   bob_the_reins = "Holding your arms out in front of you, straight and at chest level, Cross your right wrist over your left and hold them together, Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
-  lasso = "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  lasso = "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left, Lift right arm up and point right elbow diagonally to the right, "
