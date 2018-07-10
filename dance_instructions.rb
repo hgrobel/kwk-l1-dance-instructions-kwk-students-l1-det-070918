71aacd8392a254b55a51f95321b8b3b80c12942b
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:20:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,8 +1,12 @@
 
 # Code your methods below
-def dance_instructions
-  starting_stance = "Plant legs far apart, bend knees slightly and keep posture loose."
-  base_footwork = "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
-  skip_step(starting_foot) = "Lower left foot to the ground,bounce left foot back up slightly, kicking it a few inches back,left, Right, Left and Left"
-  bob_the_reins = "Holding your arms out in front of you, straight and at chest level, Cross your right wrist over your left and hold them together, Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
-  lasso = "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left, Lift right arm up and point right elbow diagonally to the right, "
+def starting_stance 
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+def base_footwork 
+  puts "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
+def skip_step(starting_foot) 
+  puts "Lower left foot to the ground,bounce left foot back up slightly, kicking it a few inches back,left, Right, Left and Left"
+def bob_the_reins 
+  puts "Holding your arms out in front of you, straight and at chest level, Cross your right wrist over your left and hold them together, Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+def lasso 
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left, Lift right arm up and point right elbow diagonally to the right, Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
