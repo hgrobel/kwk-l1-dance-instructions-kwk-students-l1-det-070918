8635bfa75e13a002a705b6017ed1a681f20b99d7
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:24:50 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -19,4 +19,9 @@ end
     puts "Be cool, be snazzy!"
 end
   
-  starting_stance base_footwork skip_step(starting_foot) bob_the_reins lasso bust_a_move
+  starting_stance 
+  base_footwork 
+  skip_step(starting_foot) 
+  bob_the_reins 
+  lasso 
+  bust_a_move
