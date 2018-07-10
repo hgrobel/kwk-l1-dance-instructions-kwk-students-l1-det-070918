d17b893abbb1449bfd59b0e61b79db67780635c9
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:34:06 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,13 +4,19 @@ def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end  
 def base_footwork 
-  puts "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
+  puts "Lift right foot."
+  puts "Return right foot to the ground."
+  puts "Finishing with a small skip-step backward."
 end
 def skip_step(starting foot)
-  puts "Lower #{starting foot} to the ground,bounce #{starting foot} back up slightly, kicking it a few inches back,left, Right, Left and Left"
+  puts "Lower #{starting foot} to the ground."
+  puts "Bounce #{starting foot} back up slightly, kicking it a few inches back."
+  puts "Left, Right, Left and Left."
 end
 def bob_the_reins 
-  puts "Holding your arms out in front of you, straight and at chest level, Cross your right wrist over your left and hold them together, Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left and hold them together." 
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 end
 def lasso 
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left, Lift right arm up and point right elbow diagonally to the right, Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
