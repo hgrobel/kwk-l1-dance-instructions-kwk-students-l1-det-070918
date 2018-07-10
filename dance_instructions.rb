901620a32b2fe3c911f1a920481d1266d0ec6e89
hgrobel <hmgrobel@gmail.com>
2018-07-10 18:17:00 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,4 +4,4 @@ def dance_instructions
   starting_stance = "Plant legs far apart, bend knees slightly and keep posture loose."
   base_footwork = "Lift right foot, return right foot to the ground, finishing with a small skip-step backward."
   skip_step(starting_foot) = "Lower left foot to the ground,bounce left foot back up slightly, kicking it a few inches back,left, Right, Left and Left"
-  
+  bob_the_reins
